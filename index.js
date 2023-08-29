b49169f05b9d5f0acfae22db6349dc69dbd1a4cb
saulius <sjsaulius@gmail.com>
2023-08-29 14:24:26 +0300

    code update

--- index.js
+++ index.js
@@ -32,13 +32,6 @@ const typeColor = {
   water: "#0190FF",
 };
 
-// set async and await function
-// getUrl();
-// async function getUrl() {
-//   const response = await fetch(url);
-//   const data = await response.json();
-// }
-
 // set pokemon id
 let pokemonId = function () {
   // set random number between 1 - 20
@@ -46,11 +39,17 @@ let pokemonId = function () {
   // set pokemon id with url
   const pokemonUrlId = url + id;
   // generate pokemonUrlId with async fetch function
+  // set async and await function
+  // getUrl();
   getUrl();
+  // async function
   async function getUrl() {
+    //   const response = await fetch(url);
     const response = await fetch(pokemonUrlId);
     const data = await response.json();
+    // Generate card
     card(data);
+    // get data stats
     name.textContent = `Name: ${data.name}`;
     hp.textContent = `HP: ${data.stats[0].base_stat}`;
     attack.textContent = `Attack: ${data.stats[1].base_stat}`;
@@ -67,31 +66,12 @@ let pokemonId = function () {
   }
 };
 
-// Generate card
-
 // make card function
 let card = function (data) {
   console.log(data);
-  // get data stats
-  // const hp = data.stats[0].base_stat;
-  // get data image
   const imgSrc = data.sprites.other.dream_world.front_default;
-
   const themeColor = typeColor[data.types[0].type.name];
   console.log(themeColor);
-
-  // get pokemon name
-  // const name = data.name;
-  // get attack stats
-  // const attack = data.stats[1].base_stat;
-  // get defence stats
-  // const defence = data.stats[2].base_stat;
-  // special-attack stats
-  // const specialAttack = data.stats[3].base_stat;
-  // special-defense stats
-  // const specialDefence = data.stats[4].base_stat;
-  // speed stats
-  // const speed = data.stats[5].base_stat;
 
   colorCard(themeColor);
   colorButton(themeColor);
